e3eff6e13d45b7fea3d501b6d5a19f759e62ae5b
travislee7 <tlee7359@live.com>
2023-08-14 02:24:17 -0700

    go

--- videoSharer/videoSharer/wsgi.py
+++ videoSharer/videoSharer/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-# Add the directory containing wsgi.py to the PYTHONPATH
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videoSharer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
